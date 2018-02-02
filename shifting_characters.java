7941823532bc6720f8aaf891a50a96603205f22a
lab1comp16 <lab1comp16@lab1comp16>
2018-02-02 18:45:13 +0530

    Adding a program which consists of character shifting in StringBuffer

---
+++ shifting_characters.java
@@ -0,0 +1,14 @@
+
+import java.io.*;
+
+public class shifting_characters 
+{
+    public static void main(String[] args) throws IOException 
+    {
+        InputStreamReader isr =new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        
+        StringBuffer sin = new StringBuffer(br.readLine());
+        int k = Integer.parseInt(br.readLine());
+    }
+}
